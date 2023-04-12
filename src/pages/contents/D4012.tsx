1addd2be75c87a678290e0ea3d35354d59253763
YonghoLee11 <kamsyyongho@naver.com>
2023-04-12 22:32:17 +0900

    20230412 study

---
+++ src/pages/contents/D4012.tsx
@@ -0,0 +1,18 @@
+export default function D4012() {
+  return (
+    <div>
+      <ul>
+        <li>**redux</li>
+        <li>설치 : </li>
+        <li>Provider : app.tsx 또는 index.tsx 파일 최상단에 위치</li>
+        <li>provider는 store를 store를 필수인자로 받는다</li>
+        <li>useSelector , useDispatch 등을 공부</li>
+        <li>store : actions.ts , ..State.ts , rootReducer, useStore</li>
+        <li>
+          useStore : provider store이고 configureStore의 reducer는
+          rootReducer형태로 인자를 받는다?
+        </li>
+      </ul>
+    </div>
+  )
+}
