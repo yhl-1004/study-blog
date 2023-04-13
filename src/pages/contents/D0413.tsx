7c5c56886d8efe4cffae11810a5a20b3ece73cf7
YonghoLee11 <kamsyyongho@naver.com>
2023-04-13 21:46:56 +0900

    useReducer 사용

---
+++ src/pages/contents/D0413.tsx
@@ -0,0 +1,15 @@
+export default function D0413() {
+  return (
+    <div>
+      오늘은 눈치보느라 멍때리는 시간이 많았다. useReducer만 해봄 useReducer는
+      React에서 제공하는 훅 중 하나로, 상태(state)와 그 상태를 변경하는
+      함수(reducer)를 사용하여 컴포넌트의 로컬 상태 관리를 할 수 있는 기능을
+      제공합니다. useReducer는 useState와 비슷한 기능을 제공하지만, 좀 더 복잡한
+      상태 관리가 필요한 경우에 유용하게 사용될 수 있습니다. useReducer는 Redux
+      라이브러리에서 영감을 받아 만들어진 훅으로, Redux의 reducer와 유사한
+      개념을 사용합니다. useReducer는 다음과 같은 형태의 함수 시그니처를 가지고
+      있습니다: const [state, dispatch] = useReducer(reducer, initialState);
+      이렇다고 한다. 뭘까.
+    </div>
+  )
+}
