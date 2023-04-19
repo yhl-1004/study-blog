9de228bd1d2801433890e8e0b50ec817b186ffda
kamsyyongho@naver.com <kamsyyongho@naver.com>
2023-04-19 18:19:06 +0900

    add reducer

---
+++ src/pages/contents/D0419.tsx
@@ -0,0 +1,9 @@
+export default function D0419() {
+  return (
+    <div>
+      오늘은 리덕스 공부를 했다.. action을 통해 reducer를 자동으로 찾아가는
+      것으로 생각이 되었다 redux내부적으로 그렇게 새용을 하는것으로 보인다..
+      다른 폴더의 reducer는 상관이 없는것일까? 내일 확인해보면 좋을듯 하다.
+    </div>
+  )
+}
